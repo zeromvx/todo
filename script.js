dfc3defe8893798507f9e9b9f4df24de820b1449
zeromvx <zeromvx@gmail.com>
2020-07-14 16:57:58 +1000

    Added deleting completed tasks after reloading

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const inputForm = document.forms.inputForm;
 const inputData = inputForm.inputData;
 let id = document.querySelectorAll('li').length + 1;
 
+
+
 inputForm.addEventListener('submit', (e) => {
     e.preventDefault();
     createTask(inputData.value);
+    saveTasks();
 });
 
 
@@ -14,17 +17,38 @@ dataBox.addEventListener('click', (e) => {
     let elem = e.target;
     if (elem.tagName === "BUTTON" && elem.classList.contains('delete')) {
         deleteTask(elem.parentNode);
+        saveTasks();
     } else if (elem.tagName === "INPUT" && elem.classList.contains('toggle')) {
         changeTaskOption(elem.parentNode);
+        saveTasks();
     }
  });
+
+
+function renderTasks() {
+    const data = localStorage.getItem("todo");
+    if (data) {
+        dataBox.innerHTML = data;
+    }
+}
+
+
+function saveTasks() {
+    let notCompletedTasks = document.querySelectorAll('li[data-done="false"]');
+    let newDataBox = '';
+    let newId = 1;
+    notCompletedTasks.forEach((item) => {
+        item.f
+        newDataBox = newDataBox + item.outerHTML + '\n';
+    });
+    localStorage.setItem("todo", newDataBox);
+}
 
 
 function createTask(name) {
 
     let newTask = document.createElement('li');
     newTask.dataset['done'] = false;
-    newTask.dataset['id'] = id;
     newTask.innerHTML = `<input type="checkbox" id="toggle-${id}" name="toggle-${id}" class="toggle">
                         <label for="toggle-${id}">${name}</label>
                         <button class='delete'></button>`;
@@ -44,3 +68,5 @@ function changeTaskOption(target) {
     toDoEl == 'false' ? target.dataset.done = 'true': target.dataset.done = 'false';
     target.classList.toggle('completed');
 }
+
+renderTasks();
